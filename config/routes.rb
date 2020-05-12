c81c91e080bfa99431d5ae5b9cae8dc67bf5bdc5
vagrant <vagrant@localhost.localdomain>
2020-05-12 10:11:52 +0000

    second commit

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
   resource :post_comments, only: [:create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
-   resources :users, only: [:show]
+  resources :users, only: [:show, :edit, :update]
 end
